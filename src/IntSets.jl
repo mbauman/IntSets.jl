d22b6ed339f59afc8cc34ab10a21fd1e77ef6a0a
Matt Bauman <mbauman@gmail.com>
2015-01-10 14:43:15 -0500

    Complete refactor of IntSet
    Use BitArray instead of using bitvector.c with haphazard added bit-array methods. Ensure that *all* codepaths for all combinations of IntSet and complement IntSets are tested.

--- src/IntSets.jl
+++ src/IntSets.jl
@@ -3,9 +3,10 @@ module IntSets
 import Base: similar, copy, eltype, sizehint!, push!, union, union!, pop!,
              delete!, empty!, setdiff, setdiff!, symdiff!, symdiff, copy!, in,
              start, next, done, first, last, isempty, shift!, length, show,
-             complement, hash
+             complement, complement!, intersect, intersect!, hash, findnextnot
 export IntSet
 
+include("bitarray.jl")
 include("intset.jl")
 
 end # module
