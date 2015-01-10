dd2329550277deb19c743eb39a2a623355141749
Matt Bauman <mbauman@gmail.com>
2015-01-10 09:22:46 -0500

    Add Base's implementation of IntSet

--- src/IntSets.jl
+++ src/IntSets.jl
@@ -1,5 +1,11 @@
 module IntSets
 
-# package code goes here
+import Base: similar, copy, eltype, sizehint!, push!, union, union!, pop!,
+             delete!, empty!, setdiff, setdiff!, symdiff!, symdiff, copy!, in,
+             start, next, done, first, last, isempty, shift!, length, show,
+             complement, hash
+export IntSet
+
+include("intset.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using IntSets
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+include("intset.jl")
