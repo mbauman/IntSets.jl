0da2dbd334f5d44edc5b67611514494724ecb4dc
Matt Bauman <mbauman@gmail.com>
2015-01-29 23:10:35 -0500

    Add comments, cleanup code, remove unused methods

--- src/intset.jl
+++ src/intset.jl
@@ -77,6 +77,14 @@ shift!(s::IntSet) = pop!(s, first(s))
 empty!(s::IntSet) = (fill!(s.bits, false); s.inverse = false; s)
 isempty(s::IntSet) = s.inverse ? length(s.bits) == typemax(Int) && all(s.bits) : !any(s.bits)
 
+# Mathematical set functions: union!, intersect!, setdiff!, symdiff!
+# When applied to two intsets, these all have a similar form:
+# - Reshape s1 to match s2, occasionally grabbing the bits that were removed
+# - Use map to apply some bitwise operation across the entire bitvector
+#   - These operations use functors to work on the bitvector chunks, so are
+#     very efficient... but a little untraditional. E.g., (p > q) => (p & ~q)
+# - If needed, append the removed bits back to s1 or invert the array
+
 union(s::IntSet, ns) = union!(copy(s), ns)
 union!(s::IntSet, ns) = (for n in ns; push!(s, n); end; s)
 function union!(s1::IntSet, s2::IntSet)
@@ -143,42 +151,36 @@ function in(n::Integer, s::IntSet)
     s.bits[n+1] != s.inverse
 end
 
-# Use the subsequent element as the state to prevent looking it up again in done
+# Use the next-set index as the state to prevent looking it up again in done
 start(s::IntSet) = next(s, 0)[2]
 function next(s::IntSet, i, invert=false)
     if s.inverse $ invert
-        n = i == typemax(Int) ? typemin(Int) : findnextnot(s.bits, i+1)
-        (i-1, ifelse(n == 0, max(i, length(s.bits))+1, n))
+        # i+1 could rollover causing a BoundsError in findnext/findnextnot
+        nextidx = i == typemax(Int) ? 0 : findnextnot(s.bits, i+1)
+        # Extend indices beyond the length of the bits since it is inverted
+        nextidx = nextidx == 0 ? max(i, length(s.bits))+1 : nextidx
     else
-        (i-1, findnext(s.bits, i+1))
+        nextidx = i == typemax(Int) ? 0 : findnext(s.bits, i+1)
     end
+    (i-1, nextidx)
 end
-done(s::IntSet, i) = (i == 0) | (i == typemin(Int))
+done(s::IntSet, i) = i <= 0
 
 # Nextnot iterates through elements *not* in the set
-startnot(s::IntSet) = next(s, 0, true)[2]
 nextnot(s::IntSet, i) = next(s, i, true)
-donenot(s::IntSet, i) = i == 0
 
-first(a::IntSet) = (s = start(a); done(a, s) ? error("set is empty") : next(a, s)[1])
-function last(s::IntSet)
+function _last(s::IntSet) # Last, but without erroring if empty
+    l = length(s.bits)
     if s.inverse
-        length(s.bits) < typemax(Int) && return typemax(Int)-1
-        n = findprevnot(s.bits, typemax(Int))-1
+        idx = l < typemax(Int) ? typemax(Int) : findprevnot(s.bits, l)
     else
-        n = findprev(s.bits, length(s.bits))-1
+        idx = findprev(s.bits, l)
     end
-    n == -1 ? error("set is empty") : n
+    return idx - 1
 end
-firstnot(a::IntSet) = (s = startnot(a); donenot(a, s) ? error("set is empty") : nextnot(a, s)[1])
-function lastnot(s::IntSet)
-    if !s.inverse
-        length(s.bits) < typemax(Int) && return typemax(Int)-1
-        n = findprevnot(s.bits, typemax(Int))-1
-    else
-        n = findprev(s.bits, length(s.bits))-1
-    end
-    n == -1 ? error("set is empty") : n
+function last(s::IntSet)
+    n = _last(s)
+    n == -1 ? throw(ArgumentError("collection must be non-empty")) : n
 end
 
 length(s::IntSet) = (n = sum(s.bits); ifelse(s.inverse, typemax(Int) - n, n))
@@ -204,25 +206,18 @@ end
 function ==(s1::IntSet, s2::IntSet)
     l1 = length(s1.bits)
     l2 = length(s2.bits)
-    l1 < l2 && return ==(s2, s1) # Swap so s1 is longer
+    l1 < l2 && return ==(s2, s1) # Swap so s1 is always equal-length or longer
 
+    # Try to do this without allocating memory or checking bit-by-bit
     if s1.inverse == s2.inverse
-        # Try to do this without allocating memory
-        if l1 == l2
-            return s1.bits == s2.bits
-        elseif (!s1.inverse && last(s1) != last(s2)) ||
-               (s1.inverse && lastnot(s1) != lastnot(s2))
-            return false
-        else
-            return s1.bits[1:l2] == s2.bits # We know the last bits are the same
-        end
+        # If the lengths are the same, simply punt to bitarray comparison
+        l1 == l2 && return s1.bits == s2.bits
+        # Otherwise check the last bit. If equal, we only need to check up to l2
+        return findprev(s1.bits, l1) == findprev(s2.bits, l2) && s1.bits[1:l2] == s2.bits
     else
         # one complement, one not. Could feasibly be true on 32 bit machines
-        if l1 < typemax(Int) || last(s1) != last(s2)
-            return false
-        else
-            return map!(!, s1.bits[1:l2]) == s2.bits
-        end
+        return l1 == typemax(Int) && # But only if at least one uses all bits
+               _last(s1) == _last(s2) && map!(!, s1.bits[1:l2]) == s2.bits
     end
 end
 
@@ -232,6 +227,8 @@ issubset(a::IntSet, b::IntSet) = isequal(a, intersect(a,b))
 
 const hashis_seed = UInt === UInt64 ? 0x88989f1fc7dea67d : 0xc7dea67d
 function hash(s::IntSet, h::UInt)
+    # Only hash the bits array up to the last-set bit to prevent extra empty
+    # bits from changing the hash result
     l = s.inverse ? findprevnot(s.bits, length(s.bits)) : findprev(s.bits, length(s.bits))
-    hash(s.bits[1:l], h) + hashis_seed + hash(s.inverse)
+    hash(s.bits[1:l], h) $ hash(s.inverse) $ hashis_seed
 end
